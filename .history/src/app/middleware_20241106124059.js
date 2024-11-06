d7780bc969c9cc50135b9b4c4275c48e161fed1b
Anthony Ricketts <mra.ricketts@gmail.com>
2024-11-06 12:43:12 +0000

    middleware

--- .history/src/app/middleware_20241106124059.js
+++ .history/src/app/middleware_20241106124059.js
@@ -1,8 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+export { auth as default } from "@/auth";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/posts(.*)"]);
 
 export const config = {
+  //the matcher will find matches for public and private routes
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
